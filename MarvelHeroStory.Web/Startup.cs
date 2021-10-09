213048fca296355f5476c5c9d04275f96d14db0a
Marcelo Guimarães <marcelo.guimaraes@indikatore.com>
2021-10-09 11:10:56 -0300

    removed unecessary using statements

--- MarvelHeroStory.Web/Startup.cs
+++ MarvelHeroStory.Web/Startup.cs
@@ -3,7 +3,6 @@ using MarvelHeroStory.Infraestructure;
 using MarvelHeroStory.Infraestructure.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
